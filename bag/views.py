989a37f928af5c1c2ac35aba81dfb97faded3b9e
KeisGSmit <kgsmit98@gmail.com>
2021-01-30 18:13:36 +0000

    Add product adjust and remove functionality in bag app

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse, HttpResponse
 from products.models import Product
 from django.contrib import messages
 
@@ -21,8 +21,6 @@ def add_to_bag(request, item_id):
     if quantity > item.stock:
         messages.error(request,'You can not order more than what we have in stock!')
     else:
-        item.stock -= quantity
-        item.save()
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
 
@@ -31,3 +29,41 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+    item = get_object_or_404(Product, pk=item_id)
+    if quantity > item.stock:
+        messages.error(request,'You can not order more than what we have in stock!')
+    elif quantity <= int("0"):
+        del bag[item_id]
+    else:
+        if item_id in list(bag.keys()):
+            bag[item_id] = quantity
+            request.session['bag'] = bag
+
+        else:
+            bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Adjust a quantity of the specified product to the shopping bag """
+    try:
+        bag = request.session.get('bag', {})
+
+        if item_id in list(bag.keys()):
+            del bag[item_id]
+            request.session['bag'] = bag
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
