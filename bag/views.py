60e438e65c0ee3bf428780a1993a4f1576bb4d9d
KeisGSmit <kgsmit98@gmail.com>
2021-01-30 20:01:55 +0000

    Add toasts template, view rendering and storage settings

--- bag/views.py
+++ bag/views.py
@@ -13,19 +13,21 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
     item = get_object_or_404(Product, pk=item_id)
     if quantity > item.stock:
-        messages.error(request,'You can not order more than what we have in stock!')
+        messages.error(request, f'You can not order more than the {product.stock} item(s) we have in stock!')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'{product.name} successfully added to bag')
 
         else:
             bag[item_id] = quantity
+            messages.success(request, f'{product.name} quantity updated')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
