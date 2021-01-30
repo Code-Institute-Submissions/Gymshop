53ee90899cdd6c031c39f37bd5246cd758b3aa94
KeisGSmit <kgsmit98@gmail.com>
2021-01-30 15:03:42 +0000

    Users can not order more than what stock we have

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
+from django.contrib import messages
 
 # Create your views here.
 
@@ -15,12 +17,17 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
-
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-
+    item = get_object_or_404(Product, pk=item_id)
+    if quantity > item.stock:
+        messages.error(request,'You can not order more than what we have in stock!')
     else:
-        bag[item_id] = quantity
+        item.stock -= quantity
+        item.save()
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
