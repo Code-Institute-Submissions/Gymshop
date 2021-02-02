34e8e53a8e1c6b6f4ade79fd8ef601e5f8464491
KeisGSmit <kgsmit98@gmail.com>
2021-02-02 14:56:35 +0000

    Test: Attempt one - stock control

--- checkout/views.py
+++ checkout/views.py
@@ -11,18 +11,12 @@ def checkout(request):
         messages.error(request, "No items in your bag")
         return redirect(reverse('products'))
 
-    # for item, value in bag:
-    #     items = get_object_or_404(Product, pk=item)
-    #     quantity = value
-    #     left_over_stock =items.stock - quantity
-    #     if left_over_stock <= 0:
-    #       messages.error("Stock has changed since you last added items to your bag")
-    #       redirect
-    #     else:
-    #        |
-    #       \ /
-    #        
-
+    for item, value in bag:
+        items = get_object_or_404(Product, pk=item)
+        quantity = value
+        if items.stock - quantity <= 0:
+            messages.error("Stock has changed since you last added items to your bag")
+            return render(request, 'bag/bag.html')
 
     print(bag)
     order_form = OrderForm()
