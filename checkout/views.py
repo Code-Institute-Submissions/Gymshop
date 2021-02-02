e38b3d0bd9e709031b32e4a5b31c9c8980673ce4
KeisGSmit <kgsmit98@gmail.com>
2021-02-02 10:26:58 +0000

    Add checkout template, CSS file and cripsy forms

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
 
+from .forms import OrderForm
 # Create your views here.
+
+
+def checkout(request, item_id):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "No items in your bag")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = '/checkout/checkout.html'
+    context = {
+        'Order_form': order_form
+    }
+    return render(request, template, context)
