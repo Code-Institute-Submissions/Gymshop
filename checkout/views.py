48b68e63769df960f3abd3d0dc7d0c3e2a7d0b4d
KeisGSmit <kgsmit98@gmail.com>
2021-02-04 09:38:43 +0000

    Style and Implement stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -22,6 +22,8 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IAvHVECTsEQEyp4uaa1Ygz9AQGg1As3mODsvFGRNI9RLcpPTPLv58rxFfRK1j831KYRBlETVze7VMlFI7qo2eIY00An40UxNo',
+        'client_secret': 'test client secret'
     }
     return render(request, template, context)
