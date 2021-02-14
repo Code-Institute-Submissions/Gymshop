5092b0e171c720905f5f6430d1bcd3a905cc5985
KeisGSmit <kgsmit98@gmail.com>
2021-02-14 20:28:38 +0000

    Viewing wishlist works

--- wishlist/views.py
+++ wishlist/views.py
@@ -9,13 +9,19 @@ from django.contrib import messages
 
 def wishlist(request):
     """ A view to return the shopping bag """
+    items = []
     profile = get_object_or_404(UserProfile, user=request.user)
-    print(profile)
     wishlist = get_object_or_404(Wishlist, user=profile)
-    print(wishlist)
     wishlist_item = get_list_or_404(WishlistItem, wishlist=wishlist)
-    print(wishlist_item)
-    return render(request, 'wishlist/wishlist.html')
+    for product in wishlist_item:
+        products = get_object_or_404(Product, pk=product.id)
+        items.append(products)
+
+    context = {
+        'wishlist': bool(wishlist),
+        'products': items,
+    }
+    return render(request, 'wishlist/wishlist.html', context)
 
 
 def add_to_wishlist(request, product_id):
