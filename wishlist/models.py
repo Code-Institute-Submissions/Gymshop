78bfbb960f200f10f83c8e5e8192ef53e67c41c7
KeisGSmit <kgsmit98@gmail.com>
2021-02-14 15:11:30 +0000

    Added model idea and removed admin

--- wishlist/models.py
+++ wishlist/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from profiles.models import UserProfile
 from products.models import Product
 
-class Wishlist(models.Model):
 
-    user = models.ForeignKey(UserProfile, null=False, blank=False, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+class Wishlist(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    wished_item = models.ManyToManyField(Product)
 
     def __str__(self):
-        return self.user
+        return self.wished_item.name
