ec8782764bc700c3767ff2c2301f84a2ee302c90
KeisGSmit <kgsmit98@gmail.com>
2021-02-18 15:22:32 +0000

    Fixed issue where user cantt comment twice (In general)

--- Gymshop/urls.py
+++ Gymshop/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path('wishlist/', include('wishlist.urls'))
+    path('wishlist/', include('wishlist.urls')),
     path('comment/', include('comments.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- comments/models.py
+++ comments/models.py
@@ -4,10 +4,10 @@ from products.models import Product
 
 
 class UserComments(models.Model):
-    user = models.OneToOneField(UserProfile, null=False, blank=False, on_delete=models.CASCADE, related_name='usercomments')
+    user = models.ForeignKey(UserProfile, null=False, blank=False, on_delete=models.CASCADE, related_name='usercomments')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE, related_name='products_comment')
     comment = models.TextField(null=False, blank=False)
-    
-    
+
+
     def __str__(self):
-        return f'Comments ({self.user})'
+        return f'Comments ({self.product})'
