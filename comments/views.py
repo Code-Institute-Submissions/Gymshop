c8227c7ac81f885a404e928c02760e6f6e2057ae
KeisGSmit <kgsmit98@gmail.com>
2021-02-19 08:46:03 +0000

    Add comment basic functionality

--- comments/views.py
+++ comments/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, redirect
 
+from .models import Product, UserComments, UserProfile
+
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
-
+@login_required
 def add_comment(request, product_id):
     redirect_url = request.POST.get('redirect_url')
+
+    user = request.user
+    product = Product.objects.get(pk=product_id)
+    comment = request.POST.get('comment')
+
+    submission = UserComments(user=user, product=product, comment=comment)
+    submission.save()
     return redirect(redirect_url)
 
 
